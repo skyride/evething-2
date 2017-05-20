0bb96ca782d861aa05128cbf937c217f93bfe05d
Adam Findlay <skylinerspeeder@gmail.com>
2017-05-20 16:33:11 +0100

    Deleted alliance update task

--- thing/tasks/esi/__init__.py
+++ thing/tasks/esi/__init__.py
@@ -1,4 +1,3 @@
 from characterinfo import *
 from character_update_spawner import *
 from market_updater import *
-from alliance_updater import *
--- thing/tasks/esi/alliance_updater.py
+++
@@ -1,9 +0,0 @@
-from .apitask import APITask
-
-
-class ESI_AllianceUpdateTask(APITask):
-    name = "thing.esi.alliance_update_task"
-    api = None
-
-    def run(self):
-        pass
