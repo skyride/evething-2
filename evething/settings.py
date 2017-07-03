ef6d1d85bad2926d457505afa910a3c8badd69cd
Adam Findlay <skylinerspeeder@gmail.com>
2017-07-03 16:02:02 +0100

    Moved ESI scopes to settings.py

--- evething/settings.py
+++ evething/settings.py
@@ -186,6 +186,31 @@ STAGGER_APITASK_STARTUP = True
 # to local_settings.
 PRICE_URL = 'http://goonmetrics.com/api/price_data/?station_id=60003760&type_id=%s'
 
+ESI_SCOPES = [
+    "esi-location.read_location.v1",
+    "esi-location.read_ship_type.v1",
+    "esi-mail.read_mail.v1",
+    "esi-mail.send_mail.v1",
+    "esi-skills.read_skills.v1",
+    "esi-skills.read_skillqueue.v1",
+    "esi-wallet.read_character_wallet.v1",
+    "esi-clones.read_clones.v1",
+    "esi-universe.read_structures.v1",
+    "esi-assets.read_assets.v1",
+    "esi-planets.manage_planets.v1",
+    "esi-fleets.read_fleet.v1",
+    "esi-ui.write_waypoint.v1",
+    "esi-fittings.read_fittings.v1",
+    "esi-markets.structure_markets.v1",
+    "esi-corporations.read_structures.v1",
+    "esi-characters.read_loyalty.v1",
+    "esi-characters.read_standings.v1",
+    "esi-industry.read_character_jobs.v1",
+    "esi-markets.read_character_orders.v1",
+    "esi-characters.read_blueprints.v1",
+    "esi-contracts.read_character_contracts.v1"
+]
+
 # load local settings
 from local_settings import *  # NOPEP8
 MANAGERS = ADMINS
