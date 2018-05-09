de6a1bc6b0f6cab969bafa5ede7ee482f5301722
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-05-09 13:00:27 +0100

    Added environment variables and new local settings file

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,36 @@ MAINTAINER Adam Findlay "skylinerspeeder@gmail.com"
 
 
 # Set up environment
-ADD . /app
 WORKDIR /app
+RUN apk add --no-cache build-base python-dev mariadb-dev postgresql-dev libffi-dev
+RUN pip install --no-cache-dir psycopg2 gunicorn wget
 
-RUN apk add --no-cache --virtual .build-deps build-base python-dev mariadb-dev postgresql-dev libffi-dev
+# Download and extract sde
+RUN wget https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2
+RUN bzip2 -v -d sqlite-latest.sqlite.bz2
+
+COPY . /app
 RUN pip install --no-cache-dir -r requirements.txt
-RUN pip install --no-cache-dir psycopg2 gunicorn
-RUN apk del .build-deps
+
+RUN cp evething/local_settings.docker.py evething/local_settings.py
+
+# Environment variables
+ENV DB_ENGINE=mysql
+ENV DB_NAME=evething2
+ENV DB_USER=evething2
+ENV DB_PASSWORD=potato
+ENV DB_HOST=127.0.0.1
+ENV DB_PORT=3306
+
+ENV ESI_URL=https://esi.tech.ccp.is
+ENV ESI_CLIENT_ID=0
+ENV ESI_SECRET_KEY=a
+ENV ESI_CALLBACK_URL=https://<your_host>/account/sso/callback/
+
+ENV REDIS_URL=redis://127.0.0.1/0
+ENV REDIS_KEY_PREFIX=evething2_
+
+
+EXPOSE 8000
+
+CMD "gunicorn" "-b 0.0.0.0:8000" "evething.wsgi:application"
--- evething/local_settings.docker.py
+++ evething/local_settings.docker.py
@@ -1,13 +1,13 @@
-import os
+# make a copy of this file as local_settings.py and edit that
 import settings
 
 # switch this to True if you want to see crash information
 # NEVER LEAVE THIS ON FOR A PUBLIC SITE
-DEBUG = True
+DEBUG = False
 
 # If you keep DEBUG set to False you will need to set allowed hosts correctly.
 # https://docs.djangoproject.com/en/1.5/ref/settings/#std%3asetting-ALLOWED_HOSTS
-ALLOWED_HOSTS = ['django', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 # this is used for a few random things, it's a pretty good idea to change it
 # http://www.miniwebtool.com/django-secret-key-generator/
@@ -21,56 +21,46 @@ ADMINS = (
 # database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'evething',                       # Or path to database file if using sqlite3.
-        'USER': 'evething',                       # Not used with sqlite3.
-        'PASSWORD': 'evething',                   # Not used with sqlite3.
-        'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR'), # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                       # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.%s' % os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     },
     # this database should contain a current version of the Static Data Export
     'import': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'sqlite-latest.sqlite',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
-}
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
-        },
     },
 }
 
 # change this to the public facing directory for static files, needed for
 # './manage.py collectstatic' to work
-STATIC_ROOT = '/evething/static-dist'
+STATIC_ROOT = '/static'
 # override the default '/static/' for static files
 #STATIC_URL = 'http://static.evething.home'
 
 # API host to use, change this if you have an API proxy handy
-API_HOST = 'https://api.eveonline.com'
+API_HOST = ''
 # Number of _permanent_ API key failures per 7 days to trigger punishment,
 # 0 to disable
 API_FAILURE_LIMIT = 3
 
+# ESI Details
+# When you create the ESI application at developers.eveonline.com you should
+# fill in this section
+ESI_URL = os.environ.get("ESI_URL")
+ESI_UPDATE_INTERVAL = 60  # How often to update in minutes
+ESI_RETRIES = 15
+ESI_DATASOURCE = "tranquility"
+ESI_CLIENT_ID = ''
+ESI_SECRET_KEY = ''
+ESI_CALLBACK_URL = os.environ.get("ESI_CALLBACK_URL")
+
 # IP addresses that will see some extra DEBUG info
 INTERNAL_IPS = (
     '127.0.0.1',
-    '192.168.59.3'
 )
 
 # Disable the password tab if you are using external auth
@@ -80,7 +70,7 @@ DISABLE_ACCOUNT_PASSWORD = False
 ONLY_NEW_APIKEYS = True
 
 # Allow new users to register
-ALLOW_REGISTRATION = False
+ALLOW_REGISTRATION = True
 
 # Default stagger APITask calls on startup
 STAGGER_APITASK_STARTUP = True
@@ -91,21 +81,21 @@ STAGGER_APITASK_STARTUP = True
 PRICE_URL = 'http://goonmetrics.com/api/price_data/?station_id=60003760&type_id=%s'
 
 # Celery broker URL - http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html#choosing-a-broker
-BROKER_URL = 'redis://%s/0' % os.environ.get('REDIS_1_PORT_6379_TCP_ADDR')
+BROKER_URL = 'amqp://guest:guest@localhost:5672/'
 
+# Cache for various things. You really want to use memcache if at all
+# possible, other caches do not guarantee atomic increments.
+#
 # https://docs.djangoproject.com/en/dev/topics/cache/
-# django-redis config
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        "LOCATION": 'redis://%s/1' % os.environ.get('REDIS_1_PORT_6379_TCP_ADDR'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'CONNECTION_POOL_KWARGS': {'max_connections': 25}
-        },
-        'KEY_PREFIX': 'dj'
+        'LOCATION': os.environ.get("REDIS_URL"),
+        'KEY_PREFIX': 'evething2_',
     }
 }
+
+
 # Use redis as the session handler
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'default'
